a5fa4ea3d7f861a60e170806f2fa6635a47ac58e
technologyremastered <44372408+technologyremastered@users.noreply.github.com>
2019-02-01 15:46:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<?php
+
+require_once "php/templates.php";
+
+page_header();
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top font-weight-bold" id="mainNav">
+  <div class="container">
+      <a class="navbar-brand js-scroll-trigger font-weight-bold" href="#page-top"><img src="img/logo.png" alt="ΚΑΤΑΤΑΚΤΗΡΙΕΣ" style="height: 50px;">
+      ΚΑΤΑΤΑΚΤΗΡΙΕΣ ΙΑΤΡΙΚΗΣ 
+  </a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+		  <li class="nav-item">
+			  <a class="nav-link js-scroll-trigger" href="#services">Υπηρεσιες</a>
+		  </li>
+      <li class="nav-item">
+			  <a class="nav-link js-scroll-trigger" href="#about">Γνωριστε μας</a>
+		  </li>
+		  <li class="nav-item">
+			  <a class="nav-link js-scroll-trigger" href="#contact">Επικοινωνια</a>
+		  </li>
+      </ul>
+  </div>
+</nav>
+
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="masthead">
+  <div class="container masthead-text text-center">
+    <h1 class="font-weight-light">ΚΑΤΑΤΑΚΤΗΡΙΕΣ ΙΑΤΡΙΚΗΣ ΠΑΤΡΑΣ</h1>
+    <p class="lead">Αναλαμβάνουμε την προετοιμασία υποψηφίων στα μαθήματα Βιολογίας, Βιοχημείας και 
+      Ιατρικής Φυσικής των κατατακτηρίων εξετάσεων της Ιατρικής Σχολής του Πανεπιστημίου Πατρών.
+    </p>
+  </div>
+  <div class="social-icons">
+    <ul class="list-unstyled text-center mb-0">
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fa fa-phone"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</header>
+
+
+
+<!-- Portfolio -->
+<section class="bg-light" id="services">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2>Τι σας παρέχουμε</h2><hr>
+              <br>
+            </div>
+          </div>
+        </div> 
+        <div class="container-fluid p-0 showcase" >
+            <div class="row no-gutters">
+    
+              <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url(./img/tile1.jpg); background-position: center;"></div>
+              <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h3>Ολιγομελή τμήματα για εξατομικευμένη μάθηση.</h3>
+               
+              </div>
+            </div>
+            <div class="row no-gutters">
+              <div class="col-lg-6 text-white showcase-img" style="background-image: url('./img/tile2.jpg'); background-position: center;"></div>
+              <div class="col-lg-6 my-auto showcase-text">
+                <h3>Μαθήματα μέσω Skype για υποψήφιους εκτός Πατρών.</h3>
+               
+              </div>
+            </div>
+            <div class="row no-gutters">
+              <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('./img/tile3.jpg'); background-position: center;"></div>
+              <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h3>Φιλικές τιμές, προσαρμοσμένες στα μέτρα σας.</h3>
+               
+              </div>
+            </div>
+			 <div class="row no-gutters">
+              <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/tile4.jpg'); background-position: center;"></div>
+              <div class="col-lg-6 my-auto showcase-text">
+                <h3>Σίγουρη επιτυχία στις εξετάσεις!</h3>
+               
+              </div>
+            </div>
+          </div>
+      </section>
+
+<!-- Basic information -->
+<section class="download bg-light" id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 my-5 text-center">
+        <img src="img/img2.png" class="img-fluid rounded" alt="study">
+      </div>
+      <div class="col-lg-8 my-auto">
+        <h2 class="section-heading">Η καλύτερη προετοιμασία για τις εξετάσεις σας!</h2>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Τα μαθήματα λαμβάνουν χώρα σε ολιγομελή τμήματα προκειμένου να επιτευχθεί η αντιμετώπιση των δυσκολιών και η ορθή καθοδήγηση του εκάστοτε υποψηφίου.</li>
+          <li class="list-group-item">Παράλληλα παρέχεται η δυνατότητα μαθημάτων <strong>εξ αποστάσεως μέσω Skype</strong>.</li>
+          <li class="list-group-item">Σκοπός μας είναι η πραγματοποίηση των ονείρων και η επίτευξη των στόχων σας.</li>
+          <li class="list-group-item">Η επιμονή, η υπομονή, η θέληση και η πίστη στον εαυτό σας ανοίγουν τον δρόμο προς την επιτυχία!</li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 mt-4">
+        <h2 class="section-heading text-center">Η ομάδα μας</h2><hr>
+        <p class="text-justify">Παρακάτω γίνεται μια σύντομη ανάλυση των χαρακτηριστικών της ομάδας μας, για περισσότερες πληροφορίες επικοινωνήστε μαζί μας.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+    <div class="col-md-6 border rounded">
+        <p class="text-center"> Βιολογία - Βιοχημεία</p>
+        <ul class="pt-2 pb-2 ">
+          <li>Απόφοιτη του Τμήματος Βιολογίας του Πανεπιστημίου Πατρών</li>
+          <li>Εμπειρία στα μαθήματα Βιολογίας και Βιοχημείας των κατατακτήριων εξετάσεων της Ιατρικής Σχολής</li>
+        </ul>
+      </div>
+      <div class="col-md-6 border rounded">
+        <p class="text-center"> Ιατρική Φυσική</p>
+        <ul class="pt-2 pb-2">
+          <li>Απόφοιτη του Τμήματος Φυσικής του Πανεπιστημίου Πατρών</li>
+          <li>Κάτοχο Μεταπτυχιακού Διπλώματος του Προγράμματος Μεταπτυχιακών Σπουδών (ΠΜΣ) Ιατρικής Φυσικής του Τμήματος Ιατρικής του Πανεπιστημίου Πατρών</li>
+          <li>Εμπειρία στη διδασκαλία του μαθήματος της Ιατρικής Φυσικής και την προετοιμασία υποψηφίων κατατακτηρίων Ιατρικής</li>
+        </ul>
+      </div>
+      </div>
+  </div>
+</section>
+
+
+
+
+<!-- Contact -->
+<section class="contact bg-light" id="contact">
+  <div class="container">
+    <h2 class="text-white">Επικοινωνήστε μαζί μας</h2><hr>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <input class="form-control" id="name" type="text" placeholder="Όνοματεπώνυμο" required="required" data-validation-required-message="Please enter your name.">
+                        <p class="help-block text-danger"></p>
+                      </div>
+                      <div class="form-group">
+                        <input class="form-control" id="email" type="email" placeholder="Email" required="required" data-validation-required-message="Please enter your email address.">
+                        <p class="help-block text-danger"></p>
+                      </div>
+                      <div class="form-group">
+                        <input class="form-control" id="phone" type="tel" placeholder="Τηλέφωνο" required="required" data-validation-required-message="Please enter your phone number.">
+                        <p class="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <textarea class="form-control" id="message" placeholder="Μήνυμα" required="required" data-validation-required-message="Please enter a message."></textarea>
+                        <p class="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="col-lg-12 text-center">
+                      <div id="success"></div>
+                      <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">ΑΠΟΣΤΟΛΗ</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div class="social-icons social-footer">
+      <ul class="list-unstyled text-center mb-0">
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fab fa fa-phone"></i>
+          </a>
+        </li>
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li>
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+     
+
+
+<?php
+page_footer();
+?>
---
+++ php/templates.php
@@ -0,0 +1,46 @@
+<?php
+
+function page_header() {
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>ΚΑΤΑΤΑΚΤΗΡΙΕΣ ΙΑΤΡΙΚΗΣ ΠΑΤΡΑΣ</title>
+        <link rel="icon" href="img/logo.png" type="image/png" sizes="16x16">
+        <!-- CSS stylesheets -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/fa.min.css" rel="stylesheet">
+        <link href="css/new-age.css" rel="stylesheet">
+        <link href="css/stylish-portfolio.css" rel="stylesheet">
+    </head>
+    <body id="page-top">
+<?php
+}
+
+function page_footer() {
+?>
+        <footer>
+            <div class="container">
+            <p>&copy; <a href="http://technologyremastered.eu/">TechnologyRemastered</a> <?php echo date("Y")?>. All Rights Reserved.</p>
+            </div>
+        </footer>
+        <!-- Bootstrap core JavaScript -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js"></script>
+        <!-- Custom scripts for this template -->
+        <script src="js/new-age.js"></script>
+		
+		<!-- Scripts for contact -->
+		<script src="js/jqBootstrapValidation.js"></script>
+		<script src="js/contact_me.js"></script>
+    </body>
+</html>
+<?php
+}
+?>
